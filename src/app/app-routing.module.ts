6f38b34a2ae9ffaaf8efcef373259d2cf10cc656
Jonarver Molina <Jonarver@molina.com>
2023-05-14 22:18:10 -0400

    resto de archivos del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoDeHeroesComponent } from './listado-de-heroes/listado-de-heroes.component';
+import { HeroProfileComponent } from './hero-profile/hero-profile.component';
+import { ModalPollComponent } from './modal-poll/modal-poll.component';
+import { PageRoutingModule} from './pages-routing.module'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+   
+    loadChildren: () => import('./pages-routing.module').then( m => m.PageRoutingModule )
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoDeHeroesComponent } from './listado-de-heroes/listado-de-heroes.component';
+import { HeroesService } from './heroes.service';
+import { HeroProfileComponent } from './hero-profile/hero-profile.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { ModalPollComponent } from './modal-poll/modal-poll.component';
+import { CapitalizePipe } from './capitalize.pipe';
+import { NgxsModule } from '@ngxs/store';
+
+import { HeroeState } from './heroe/state/heroe.state'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoDeHeroesComponent,
+    HeroProfileComponent,
+    SpinnerComponent,
+    ModalPollComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgxsModule.forRoot(
+      [
+        HeroeState
+      ]
+    )
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
