4dc404f85ca282ece8657325737e894dec8fb410
Jonarver Molina <Jonarver@molina.com>
2023-05-14 22:10:50 -0400

    agregar manejo de estado

---
+++ src/app/heroe/state/heroe.model.ts
@@ -0,0 +1,31 @@
+export interface HeroesStateModel {
+    posts: Heroe[];
+  }
+  
+  export interface Heroe {
+     id: string;
+     name: string;
+     description: string;
+     modified: Date;
+     thumbnail: Object;
+     resourceURI: string;
+     teamColor: string;
+  }
+
+  export interface listHeroes{
+   beforePage:number
+   count:number
+   heroes:Heroe[]
+   limit:number
+   nextPage:number
+   offset:number
+   page:number
+   total:number
+   totalPages:number
+  }
+
+  export interface HeroeTeamColor {
+   id: string;
+   calor: string;
+   
+}
